70119e41a2dbe2321994527a3757b58bab665bf5
ViktorKula <vicyru@yandex.ru>
2025-05-14 23:35:40 +0300

    add app

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+
+import classes from "./Header.module.scss";
+
+import imgLogo from "../../img/Header/logo.module.png";
+import UserPanel from "./UserPanel/UserPanel";
+
+const Header: React.FC = () => {
+
+    return (
+        <div className={classes.header_container}>
+            <div className={classes.header}>
+                <div className={classes.upper}>
+                    <div className={classes.logo_div}>
+                        <Link to="/">
+                            <img src={imgLogo} alt="Site logo"/>
+                        </Link>
+                    </div>
+                    <div className={classes.header_title}>
+                        <h1>+7-8352-20-12-09, telegram</h1>
+                        </div>
+                    <UserPanel />
+                </div>
+                <div className={classes.header_sub_title}>
+                    <h2>Электронная сервисная книжка "МойСилант"</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
